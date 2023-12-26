101dbd7909bde5d58478caac0e7ba6abcdc2e1a8
Esther White <monacodelisa@gmail.com>
2023-12-26 16:45:00 +0200

    jobs component

--- dummyRESTfrontend/src/app/components/home/home.component.ts
+++ dummyRESTfrontend/src/app/components/home/home.component.ts
@@ -12,32 +12,10 @@ import { Job } from '../../models/job';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
-  jobsParam: string | null = "";
-  jobs: Job[] = [];
 
   constructor(private http: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      // if (params.has('users')) {
-      //   this.usersParam = params.get('users');
-      //   this.jobsParam = null;
 
-      //   this.http.get<User[]>("https://monacodelisa-node-express.onrender.com/dummy-rest-api/users").subscribe(data => {
-      //     this.users = data;
-      //   });
-      // } else if (params.has('jobs')) {
-      //   this.jobsParam = params.get('jobs');
-      //   this.usersParam = null;
-
-      //   this.http.get<Job[]>("https://monacodelisa-node-express.onrender.com/dummy-rest-api/jobs").subscribe(data => {
-      //     this.jobs = data;
-      //   });
-      // } else {
-      //   // Handle other cases or set default values if needed
-      //   this.usersParam = null;
-      //   this.jobsParam = null;
-      // }
-    });
   }
 }
--- dummyRESTfrontend/src/app/components/jobs/jobs.component.ts
+++ dummyRESTfrontend/src/app/components/jobs/jobs.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Job } from '../../models/job';
 
 @Component({
   selector: 'app-jobs',
@@ -7,6 +9,15 @@ import { Component } from '@angular/core';
   templateUrl: './jobs.component.html',
   styleUrl: './jobs.component.scss'
 })
-export class JobsComponent {
+export class JobsComponent implements OnInit {
+  jobs: Job[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<Job[]>("https://monacodelisa-node-express.onrender.com/dummy-rest-api/jobs").subscribe(data => {
+      this.jobs = data;
+    })
+  }
 
 }
