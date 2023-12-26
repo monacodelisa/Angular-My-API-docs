2398f0fa2fc686e0ce9599c9d93a3a9f01552b87
Esther White <monacodelisa@gmail.com>
2023-12-26 12:55:40 +0200

    layout

--- dummyRESTfrontend/src/app/app.component.ts
+++ dummyRESTfrontend/src/app/app.component.ts
@@ -1,14 +1,49 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { MediaMatcher } from "@angular/cdk/layout";
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: "app-root",
+	standalone: true,
+	imports: [
+		CommonModule,
+		RouterLink,
+		RouterLinkActive,
+		RouterOutlet,
+		MatToolbarModule,
+		MatSidenavModule,
+		MatListModule,
+		MatButtonModule,
+		MatIconModule,
+	],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss",
 })
-export class AppComponent {
-  title = 'dummyRESTfrontend';
+export class AppComponent implements OnInit, OnDestroy {
+	title = "dummyRESTfrontend";
+  opened!: boolean;
+	mobileQuery: MediaQueryList;
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+		this.mobileQuery = media.matchMedia("(max-width: 600px)");
+		this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+		this.mobileQuery.addListener(this._mobileQueryListener);
+	}
+
+  ngOnInit(): void {
+    this.opened = true;
+  }
+
+	ngOnDestroy(): void {
+		this.mobileQuery.removeListener(this._mobileQueryListener);
+	}
 }
